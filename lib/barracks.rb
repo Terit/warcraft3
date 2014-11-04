15232e7456a1beda473c98a947510cfa2dfeaeaa
Andy Theriault <artheriault@gmail.com>
2014-11-04 23:40:49 +0000

    Simple Warcraft 3 units.

---
+++ lib/barracks.rb
@@ -0,0 +1,36 @@
+require_relative 'footman'
+require_relative 'peasant'
+
+class Barracks
+
+  attr_reader :gold, :food
+
+  def initialize
+    @gold = 1000
+    @food = 80
+  end
+
+  def train_footman
+    if can_train_footman?
+      @gold -= 135
+      @food -= 2
+      Footman.new
+    end
+  end
+
+  def can_train_footman?
+    (food < 2 || gold < 135) ? false : true
+  end
+
+  def train_peasant
+    if can_train_peasant?
+      @gold -= 90
+      @food -= 5
+      Peasant.new
+    end
+  end
+
+  def can_train_peasant?
+    (food < 5 || gold < 90) ? false : true
+  end
+end
